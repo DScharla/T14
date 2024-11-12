146078b1beaef00a9b008b921f548a85b803d863
DScharla <dsan62373@edu.ucl.dk>
2024-11-12 09:40:55 +0100

    Igang med at implementere repo

--- Eksamensprojekt/Model/IRepository.cs
+++ Eksamensprojekt/Model/IRepository.cs
@@ -4,23 +4,95 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
+using Microsoft.Data.SqlClient;
+using Azure.Core.GeoJson;
 
 namespace Eksamensprojekt.Model
 {
     public interface IRepository <T> where T : class
     {
-        ObservableCollection<T> GetAll();
+        ObservableCollection<T> GetAll( string getAllQuery, string connectionString) 
+        {
+            ObservableCollection<T> all = new ObservableCollection<T>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(getAllQuery, connection);
+                connection.Open();
+                
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        all.Add(
+                            FromStringToType(reader)
+                            );
+                    }
+                }
+            }
+            return all;
+        }
 
         string GetStringFromDB();
 
-        List<T> FromStringToType();
+        T FromStringToType(SqlDataReader reader);
         
-        T GetById(string id);
+        T GetById(string id, string getByIdQuery, string connectionString)
+        {
+            T getById = null;
 
-        T Add(T entity);
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
 
-        T Remove(T entity);
+                SqlCommand command = new SqlCommand(getByIdQuery, connection);
+                command.Parameters.AddWithValue("@Id", id);
+                connection.Open();
 
-        T Update(T entity);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+
+                    if (reader.Read())
+                    {
+                        getById = FromStringToType(reader);
+                    }
+
+                }
+
+            }
+
+            return getById;
+
+        }
+
+        void Add(T entity, string connectionString, string AddQuery)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                SqlCommand command = new SqlCommand(AddQuery, connection);
+                //command.Parameters.AddWithValue("@Id", entity.id);
+                connection.Open();
+                command.ExecuteNonQuery();
+
+            }
+        }
+
+        void Remove(T entity, string connectionString, string RemoveQuery)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                SqlCommand command = new SqlCommand(RemoveQuery, connection);
+                //command.Parameters.AddWithValue("@Id", entity.id);
+                connection.Open();
+                command.ExecuteNonQuery();
+
+            }
+        }
+
+        void Update(T entity)
+        {
+
+        }
     }
 }
