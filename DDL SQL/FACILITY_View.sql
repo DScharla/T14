e71798d48a24b3ff7f9daa5037be86aa7fd4293c
DScharla <dsan62373@edu.ucl.dk>
2024-11-18 11:25:46 +0100

    View

--- DDL SQL/FACILITY_View.sql
+++ DDL SQL/FACILITY_View.sql
@@ -5,8 +5,8 @@ SELECT TOP (1000) [OverflowID]
       ,[FacilityID]
       ,[IncidentID]
   FROM [VF].[dbo].[OVERFLOW]
-
-
+  
+CREATE VIEW vwFacility AS
 SELECT        dbo.FACILITY.FacilityID, dbo.FACILITY.Name AS FacilityName, dbo.FACILITY.NumberOfIncidents, dbo.FACILITY.TotalOverflow, dbo.FACILITY.UDL, dbo.FACILITY.OBNumber, dbo.FACILITY.MinimumPoolSize, 
                          dbo.SYSTEM.Name AS SystemName
 FROM            dbo.FACILITY INNER JOIN
